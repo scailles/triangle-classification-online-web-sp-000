b8afde1d3f0b2148fa33b6fe8c6693714b3ab359
Shaina C <scailles23@gmail.com>
2021-01-14 05:11:58 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,7 +10,8 @@ end
 
 
 def kind
-  if (@s1 <=0) || (@s2 <=0) || (@s3 <= 0)
+  sides= [@s1, @s2, @s3]
+  if sides.any?{|side| side =< 0}
     raise TriangleError
     elsif (@s1 + @s2 <= @s3) or (@s1+@s3 <= @s2) or (@s2+@s3 <= @s1)
     raise TriangleError
