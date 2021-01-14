42347a3ed03d786e9af0bfa46dee42086734c373
Shaina C <scailles23@gmail.com>
2021-01-14 04:54:32 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,18 +11,21 @@ def kind()
   if (@s1 <=0) || (@s2 <=0) || (@s3 <= 0)
     raise TriangleError
     elsif (@side_1+@side_2 <= @side_3) || (@side_1+@side_3 <= @side_2) || (@side_2+@side_3 <= @side_1)
+    raise TriangleError
   else 
     if (@s1 == @s2) && (@s2 == @s3)
       :equilateral
     elsif (@s1 == @s2) or (@s1 == @s3)
+      :isosceles
+      
+     end
+   end
   end
-end
-end
 
 end
+  
+  
   
   
 class TriangleError < StandardError
-
-  
 end
