419aa2a7fc5bc2f5a18180929cc598d40160e6d0
Shaina C <scailles23@gmail.com>
2021-01-14 05:07:25 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -12,7 +12,7 @@ end
 def kind
   if (@s1 <=0) || (@s2 <=0) || (@s3 <= 0)
     raise TriangleError
-    elsif (@s1 + @s2 <= @s3) || (@s1+@s3 <= @s2) || (@s2+@s3 <= @s1)
+    elsif (@s1 + @s2 <= @s3) or (@s1+@s3 <= @s2) or (@s2+@s3 <= @s1)
     raise TriangleError
   else 
     if (@s1 == @s2) && (@s2 == @s3)
