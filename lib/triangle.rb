0c237732397f962f07013b976b42ea22c3048ea3
Shaina C <scailles23@gmail.com>
2021-01-14 05:36:15 +0000

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,41 @@
 class Triangle
-  # write code here
+  
+  attr_reader :s1, :s2, :s3 
+  
+def initialize(s1,s2,s3)
+  @s1 = s1
+  @s2 = s2
+  @s3 = s3
+  
+  @sides = [@s1, @s2, @s3]
+
+end
+
+
+def kind
+  real_triangle
+    if (@s1 == @s2) && (@s2 == @s3)
+      :equilateral
+    elsif (@s1 == @s2) || (@s1 == @s3) || (@s2 == @s3)
+      :isosceles
+    else 
+      :scalene
+     end
+   end
+  
+  
+  
+  def real_triangle
+    if @sides.any?{|side| side == 0} || ((@s1 + @s2) <= @s3) || @sides.any?{|side| side <= 0} || ((@s1 + @s3) <= @s2) || ((@s2 + @s3) <= @s1)
+      raise TriangleError
+    end
+  end
+  
+  #|| ((@s1 + @s2) <= @s3) || sides.all?{|side| side = 0}
+      #raise TriangleError
+ 
+  
+class TriangleError < StandardError
+end
+
 end
